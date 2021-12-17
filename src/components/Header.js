d03f0cf60280b33b8ca56bdb2bef1961cc44371c
Tian Xu <xueqingxu0302@gmail.com>
2021-12-17 12:54:20 -0700

    commit tree 9a4ba70ad6f7cabb38ea92a4dd0194f4ae9e5bd3

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+
+
+
+function Header({isDarkMode,onDarkModeClick}){
+    return (
+    <>
+    <h2>Shopster</h2>
+    <button onClick={onDarkModeClick}>
+      {isDarkMode ? "Dark" : "Light"} Mode
+    </button>
+    </>)
+}
+    
+export default Header
+
